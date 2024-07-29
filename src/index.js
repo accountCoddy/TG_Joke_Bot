d7f2de42e9cb87426ae9aa88e4e6ba23ec0851f4
Dianom23 <dmitry.sh23@gmail.com>
2024-07-29 17:23:34 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// Вставьте сюда ваш токен от BotFather
+const token = '7380815618:AAHJLWOul_vrhmpxCyHK20qvp_cMvyRdrLY';
+
+// Создание экземпляра бота
+const bot = new TelegramBot(token, { polling: true });
+
+// Команда /start
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привет! Я ваш Telegram бот.');
+});
+
+// Команда /joke
+bot.onText(/\/joke/, (msg) => {
+    const chatId = msg.chat.id;
+    const jokes = [
+        'Почему программисты не любят природу? Слишком много багов.',
+        'Что сказал программист после того, как его код заработал? "О, так он вообще-то работает!"',
+        'Почему программисты предпочитают темные комнаты? Потому что свет притягивает багов.',
+        'Какое животное ненавидят программисты? Баг-зайцы.',
+        'Почему у программистов всегда беспорядок на столе? Они хранят все в стеке.'
+    ];
+    const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+    bot.sendMessage(chatId, randomJoke);
+});
+
+console.log('Бот запущен');
